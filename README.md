75391c3e18b409144b62eb2417a92e84cfdaf12d
gaole <1033727528@qq.com>
2019-12-05 19:29:43 +0800

    learn react day02

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 1. 学习 react 的组件化 css
 2. ... 代表展开运算符,如果 list 中有内容，则可以将该 list 中的内容直接全部展示出来
 3. 在需要绑定声明的组件到标签上时，使用 bind() 方法
+4. 在 react 中遍历集合时，需要给定遍历出来的标签唯一的 key 值，否则浏览器 console 控制台会出现警告：Warning: Each child in a list should have a unique "key" prop.
+# learn day02
+5. 对构造方法中声明的数据进行拷贝 const list = [...this.state.list];
+6. 在对 state 中的集合进行修改时，不建议直接操作 state 中的集合，而是 const 拷贝出一个集合的副本进行操作，这样待办的可调式性和性能都是最优的
+7. MyApp.js 中的 js 语法，属于 es6 的语法
+8. react 中存在 “父子” 组件，父组件通过属性的形式向子组件传递参数。子组件通过 props 接受父组件传递过来的参数
--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+import TodoItem from './TodoItem'
 
 class MyApp extends React.Component {
 
@@ -9,17 +10,35 @@ class MyApp extends React.Component {
     super(props);
     this.state = {
       list: [
-        'learn react',
-        'learn englist',
-        'learn vue'
-      ]
+        // 'learn react',
+        // 'learn englist',
+        // 'learn vue'
+      ],
+      inputValue: ''
     }
   }
 
   handlerBtnClick () {
     this.setState({
       // ... 展开运算符,在这里指：'learn react','learn englist','learn vue'
-      list: [...this.state.list,'hello world']
+      list: [...this.state.list,this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handlerInputChange (e) {
+    this.setState ({
+      inputValue: e.target.value
+    })
+  }
+
+  handlerItemClick (index) {
+    const list = [...this.state.list];
+    // 根据索引移除集合中的数据
+    list.splice(index,1);
+    // 将更新后的集合，重新赋值给原始集合
+    this.setState({
+      list: list
     })
   }
 
@@ -27,13 +46,14 @@ class MyApp extends React.Component {
     return (
       <div>
         <div>
-          <input />
+          <input value={this.state.inputValue} onChange={this.handlerInputChange.bind(this)}/>
           {/* bind(this)：将当前组件绑定到 button 标签上 */}
           <button onClick={this.handlerBtnClick.bind(this)}>add</button>
         </div>
         <ul>
-          { this.state.list.map((item) => {
-            return <li>{item}</li>
+          { this.state.list.map((item,index) => {
+            return <TodoItem onClick={this.handlerItemClick.bind(this,index)} key={index} content={item} />
+            // return <li onClick={this.handlerItemClick.bind(this,index)} key={index}>{item}</li>
           })}
         </ul>
       </div>
